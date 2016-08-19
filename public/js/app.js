b12eff03ceb3a08cb4a9c73dae1ddeeb3398f113
aadams22 <ashleigh.adams22@gmail.com>
2016-08-19 14:42:53 -0500

    code cleaning

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,22 @@
 $(function(){
 
+//======================
+//VARIABLE DEFINITIONS
+//======================
 	var socket = io();
-  var m = $("<li class='l'>");
-  var em = null;
-  var l = null;
-  var dm = null;
+  var m      = $("<li class='l'>");
+  var l      = null;
+  var em     = null;
+  var dm     = null;
 
 
+//======================
+//FUNCTIONALITY
+//======================
   $('form').submit(function(){
+    //sends value of user entered text to backend over ws to be encrypted and submitted 
     socket.emit('Chat Message', $('#m').val());
+    //resets value of submition input to empty;
     $('#m').val('');
     return false;
   });
@@ -16,38 +24,38 @@ $(function(){
 
   $('ul').on('mouseenter', '.l', 
     function(e){
-    em = $(this).text();
-    l = $(this);
-    console.log('in ', em);
-    decryptMsg(em);
-    console.log('inner dm', dm);
-    
+      em = $(this).text();
+      l = $(this);
+      decryptMsg(em);
   })
   .on('mouseleave', '.l', function(e){
-    console.log('out', em);
+    //reset inner text to original encryption
     l.text(em);
+    //reset values to null
     l = null;
     em = null;
     dm = null;
   });
 
 
-
-
+//======================
+//FUNCTION DEFINITIONS
+//======================
+  //sends encrypted value over hovered item to backend to be decrypted
   function decryptMsg(em){
     socket.emit('Decrypt Message', em);
   };
   
-
-
+//======================
+//SOCKETS
+//======================
   socket.on('Chat Message', function(msg){
     $('#messages').append(m.text(msg));
     m = $("<li class='l'>");
   });
 
   socket.on('Recieve Decrypted Msg', function(dm){
-    console.log('recieved decrypted: ', dm);
-    console.log(l);
+    //sets inner of hovered li to decrypted text
     l.text(dm);
   });
 
