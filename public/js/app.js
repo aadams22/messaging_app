3c9bfe64dc8b6838ab26febd68c5939446baae4f
aadams22 <ashleigh.adams22@gmail.com>
2016-08-19 14:33:53 -0500

    hover encryption working

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,6 @@ $(function(){
 
 	var socket = io();
   var m = $("<li class='l'>");
-  var li = $('ul').find('.l');
   var em = null;
   var l = null;
   var dm = null;
@@ -14,19 +13,22 @@ $(function(){
     return false;
   });
 
-  $('ul').on('click', '.l', function(){
-    console.log('hi');
-  });
 
   $('ul').on('mouseenter', '.l', 
     function(e){
     em = $(this).text();
-
+    l = $(this);
     console.log('in ', em);
     decryptMsg(em);
+    console.log('inner dm', dm);
+    
   })
   .on('mouseleave', '.l', function(e){
-    console.log('out');
+    console.log('out', em);
+    l.text(em);
+    l = null;
+    em = null;
+    dm = null;
   });
 
 
@@ -44,9 +46,9 @@ $(function(){
   });
 
   socket.on('Recieve Decrypted Msg', function(dm){
-    // console.log('recieved decrypted: ', dm);
-    console.log(dm);
-    
+    console.log('recieved decrypted: ', dm);
+    console.log(l);
+    l.text(dm);
   });
 
 });
